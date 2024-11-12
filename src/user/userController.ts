2d2c2d662ddf8f8028eadaf88ef9998dd5b05843
vivekpayal1 <vivekpayal8954@gmail.com>
2024-11-12 19:15:23 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     const error = createHttpError(400, "All Field Required!");
+    return next(error);
+  }
+  // DB Call
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User Already Exists with this Email");
     return next(error);
   }
   res.json({ message: "User Created" });
---
+++ src/user/userModel.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model<User>("User", userSchema);
