c0f5686ca8f802d37f3644f299309e7868e464e7
vivekpayal1 <vivekpayal8954@gmail.com>
2024-11-12 18:59:21 +0530

    add user register validation

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 
-
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome To Elib Apis" });
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All Field Required!");
+    return next(error);
+  }
   res.json({ message: "User Created" });
 };
 export { createUser };
